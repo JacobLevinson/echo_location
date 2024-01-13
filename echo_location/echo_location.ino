d2b494d6836a09a3112215ee8659e549b6e2097c
Jacob Levinson <96364327+JacobLevinson@users.noreply.github.com>
2024-01-12 20:17:51 -0800

    Added ino file

---
+++ echo_location/echo_location.ino
@@ -0,0 +1,13 @@
+// echo location Idea Hacks LA 2024 Project
+
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
